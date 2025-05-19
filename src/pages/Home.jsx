95ef5151396a48597549ef07d54fb4be06e70ae0
devil-hash <sakthigowtham008@gmail.com>
2025-05-19 10:46:45 +0530

    Day 3 jenkins check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
             </div>
             <div className="col-lg-6" data-aos="fade-right">
               <h1 className="typing-text">
-                Hello People, I'm <span className="highlight">Gowtham</span>
+                Hy there, I'm <span className="highlight">Gowtham</span>
               </h1>
               <div className="hero-description">
                 <p>
